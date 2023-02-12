a357bfd09119976fe1ede97b5a8e17e2ba31750f
DmitryRyzhik <rdssdr03@gmail.com>
2023-02-12 23:04:55 +0800

    Домашнее задание к семинару 1

---
+++ 1/Program.cs
@@ -0,0 +1,23 @@
+﻿// Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+// a = 5; b = 7 -> max = 7
+// a = 2 b = 10 -> max = 10
+// a = -9 b = -3 -> max = -3
+
+Console.Write("Введите числa a: ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите числa b: ");
+int num2 = Convert.ToInt32(Console.ReadLine());
+if (num1 == num2)
+{
+    System.Console.Write("a равно b");
+    return;
+}
+
+if (num1 >= num2)
+{
+System.Console.Write("a больше чем b");
+}
+else
+{
+    System.Console.Write("b больше чем a");
+}
---
+++ 2/Program.cs
@@ -0,0 +1,23 @@
+﻿// Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+// 2, 3, 7 -> 7
+// 44 5 78 -> 78
+// 22 3 9 -> 22
+
+Console.Write("Введите число a: ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число b: ");
+int num2 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число c: ");
+int num3 = Convert.ToInt32(Console.ReadLine());
+if (num1 >= num2 && num1 >= num3)
+{
+    System.Console.Write("Максимальное число = а");
+}
+else if (num2 >= num3)
+{
+    System.Console.Write("Максимальное число = b");
+}
+else
+{
+    System.Console.Write("Максимальное число = c");
+}
